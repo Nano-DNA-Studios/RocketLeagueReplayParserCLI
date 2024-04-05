9e08b8f051125f7c08e21f256c9db147fff003d0
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-04-05 17:59:55 -0400

    First Command Reset

--- RocketLeagueReplayExtractorCLI/Program.cs
+++ RocketLeagueReplayExtractorCLI/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace RocketLeagueReplayExtractorCLI
+﻿using RocketLeagueReplayExtractorCLI;
+using RocketLeagueReplayParserAPI;
+
+namespace RocketLeagueReplayExtractorCLI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            string fullFilePath = Path.GetFullPath(args[0]);
+
+
+            if (File.Exists(fullFilePath))
+            {
+                Replay replay = new Replay(fullFilePath);
+
+                if (args[1] == "Goals")
+                {
+                    Console.WriteLine($"Blue Team Goals: {replay.BlueTeamGoals}");
+                    Console.WriteLine($"Orange Team Goals: {replay.OrangeTeamGoals}");
+                }
+
+            }
+
+
+
+
+
+
+
             Console.WriteLine("Hello, World!");
             Console.WriteLine($"Args : {args[0]}");
 
