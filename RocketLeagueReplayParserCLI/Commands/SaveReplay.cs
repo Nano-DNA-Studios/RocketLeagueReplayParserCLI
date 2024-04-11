09a509fdcb26182d9aef72648b2e00b195766bcb
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-04-10 21:51:57 -0400

    I think I foiund something

--- RocketLeagueReplayParserCLI/Commands/SaveReplay.cs
+++ RocketLeagueReplayParserCLI/Commands/SaveReplay.cs
@@ -1,10 +1,6 @@
 ﻿using DNA_CLI_Framework.Commands;
 using DNA_CLI_Framework.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static RocketLeagueReplayParserAPI.Replay;
 
 namespace RocketLeagueReplayParserCLI.Commands
 {
@@ -24,6 +20,51 @@ namespace RocketLeagueReplayParserCLI.Commands
         /// <inheritdoc/>
         public override void Execute(string[] args)
         {
+            int lastFrameNumber = 0;
+            foreach (GameObjectState gameObjectState in Data.Replay.BallPosition)
+            {
+                try
+                {
+
+                    if (gameObjectState.RigidBody != null && gameObjectState.RigidBody.LinearVelocity != null)
+                    {
+                        float x, y, z;
+                        double v;
+
+                        x = gameObjectState.RigidBody.LinearVelocity.X / 10000;
+                        y = gameObjectState.RigidBody.LinearVelocity.Y / 10000;
+                        z = gameObjectState.RigidBody.LinearVelocity.Z / 10000;
+
+                        v = Math.Sqrt(x * x + y * y + z * z);
+
+                        v = v * 3.6;
+
+                        Console.WriteLine($"Ball Speed (km/h) at frame ({gameObjectState.FrameNumber}): {v}   (Frame Delta = {gameObjectState.FrameNumber - lastFrameNumber}) (Time : {gameObjectState.Time})  (X :{x}   Y :{y}  Z :{z})   (Pos: {gameObjectState.RigidBody.Position.ToString()}");
+
+                        lastFrameNumber = gameObjectState.FrameNumber;
+
+
+
+
+                    }
+
+                }
+                finally
+                {
+
+                }
+
+            }
+
+            foreach (BallHit ballHit in Data.Replay.BallHits)
+            {
+                if (ballHit.team ==0)
+                    Console.WriteLine($"Ball Hit at frame ({ballHit.frameNumber}) by Blue Team");
+                else
+                    Console.WriteLine($"Ball Hit at frame ({ballHit.frameNumber}) by Orange Team");
+            }
+
+
             if (args.Length == 0)
             {
                 Data.Replay.SaveReplayAsJSON();
